94e9795054f856d750c0d1fdf366107586455c7c
Meon <test@test.test>
2016-09-21 13:34:11 +1000

    Initial commit

---
+++ app/app.jsx
@@ -0,0 +1,18 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+var {Route, Router, IndexRoute, hashHistory, browserHistory} = require('react-router');
+// import {Route, Router, IndexRoute, hashHistory, browserHistory} from 'react-router';
+
+// Load Foundation
+// require('style!css!foundation-sites/dist/foundation.min.css') // replaced by sassLoader in webpack.config.js
+
+// start Foundation
+$(document).foundation();
+
+// Load css
+require('style!css!sass!appStyles')
+
+ReactDOM.render(
+    <p> React Boilerplate 3 </p>,
+    document.getElementById('app')
+);
